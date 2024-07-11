97244fde3eb4b19db3dcd77cb30bb31246d6ce5f
Vishal-Moreyeahs <vishal.pawar@moreyeahs.com>
2024-07-11 16:05:13 +0530

    Calling unit updated in controller

--- Fincompare.Api/Controllers/Admin/CountryController.cs
+++ Fincompare.Api/Controllers/Admin/CountryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Fincompare.Application.Repositories;
+using Fincompare.Application.Request.CountryRequest;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fincompare.Api.Controllers.Admin
@@ -7,6 +9,46 @@ namespace Fincompare.Api.Controllers.Admin
     [ApiController]
     public class CountryController : ControllerBase
     {
+        private readonly ICountryServices _countryServices;
 
+        public CountryController(ICountryServices countryServices)
+        {
+            _countryServices = countryServices;
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> CreateCompany(CountryRequest request)
+        {
+            return Ok(await _countryServices.AddCountry(request));
+        }
+
+        [HttpPost]
+        [Route("update")]
+        public async Task<IActionResult> UpdateCompany(CountryRequest request)
+        {
+            return Ok(await _countryServices.UpdateCountry(request));
+        }
+
+        [HttpDelete]
+        [Route("remove")]
+        public async Task<IActionResult> DeleteCompanyById(string country3iso)
+        {
+            return Ok(await _countryServices.RemoveCountry(country3iso));
+        }
+
+        [HttpGet]
+        [Route("get-all")]
+        public async Task<IActionResult> GetAllCompany()
+        {
+            return Ok(await _countryServices.GetAllCountry());
+        }
+
+        [HttpGet]
+        [Route("get-by-country3Iso")]
+        public async Task<IActionResult> GetCompanyById(string country3iso)
+        {
+            return Ok(await _countryServices.GetCountryByCountryName(country3iso));
+        }
     }
 }
