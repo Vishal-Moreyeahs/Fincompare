69fc35a0e05e47772e3d387ddb9e3755bf1997e9
Vishal-Moreyeahs <vishal.pawar@moreyeahs.com>
2024-07-16 19:07:38 +0530

    Create Response model for Maintaing Graph statistics of mid-market

---
+++ Fincompare.Application/Response/MarketRateResponse/MarketRateStatisticResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincompare.Application.Response.MarketRateResponse
+{
+    public class MarketRateStatisticsData
+    {
+        public List<RateDataModel> RateWeek { get; set; }
+        public List<RateDataModel> Rates2Weeks { get; set; }
+        public List<RateDataModel> RatesMonths { get; set; }
+         
+    }
+
+    public class RateDataModel
+    { 
+        public double Rate { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
