7636768addff113b661d2180c0c4c091dcd51114
Vishal-Moreyeahs <vishal.pawar@moreyeahs.com>
2024-07-17 16:55:44 +0530

    Refactor the code

--- Fincompare.Application/Repositories/IMerchantServices.cs
+++ Fincompare.Application/Repositories/IMerchantServices.cs
@@ -6,7 +6,7 @@ namespace Fincompare.Application.Repositories
     public interface IMerchantServices
     {
         Task<ApiResponse<MerchantDto>> OnboardMerchant(AddMerchantRequest model);
-        Task<ApiResponse<string>> EditMerchantProfile(UpdateMerchantRequest model);
+        Task<ApiResponse<MerchantDto>> EditMerchantProfile(UpdateMerchantRequest model);
         Task<ApiResponse<IEnumerable<MerchantDto>>> GetAllMerchants();
         Task<ApiResponse<MerchantDto>> GetMerchantByMerchantId(int merchantId);
         Task<ApiResponse<MerchantDto>> GetMerchantByUserId(int userId);
