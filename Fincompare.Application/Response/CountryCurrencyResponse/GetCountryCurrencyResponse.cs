0ad3aae3076879537253e7abb9e32dbba9b9d4b2
Vishal-Moreyeahs <vishal.pawar@moreyeahs.com>
2024-07-19 01:01:09 +0530

    refacotr the country currency code

---
+++ Fincompare.Application/Response/CountryCurrencyResponse/GetCountryCurrencyResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincompare.Application.Response.CountryCurrencyResponse
+{
+    public class GetCountryCurrencyResponse
+    {
+        public int CountryCurrencyID { get; set; }
+        public string CountryIso3 { get; set; }
+        public string CountryIso2 { get; set; }
+        public bool IsPrimary { get; set; }
+        public string Category { get; set; }
+        public bool Status { get; set; }
+    }
+}
