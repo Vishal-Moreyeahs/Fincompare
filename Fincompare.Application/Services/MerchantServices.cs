6050fb98112a7ebbfd63250636ffd3ed611fbddf
Ravi Vyas Vyas <ravi.vyas@moreyeahs.in>
2024-08-06 17:12:03 +0530

    commit bug resolved

--- Fincompare.Application/Services/MerchantServices.cs
+++ Fincompare.Application/Services/MerchantServices.cs
@@ -102,7 +102,7 @@ namespace Fincompare.Application.Services
         }
 
 
-        public async Task<ApiResponse<IEnumerable<MerchantDto>>> GetAllMerchants(int? groupMerchantId, int? merchantId,string? merchantType ,string? countryIso3, bool? status)
+        public async Task<ApiResponse<IEnumerable<MerchantDto>>> GetAllMerchants(int? groupMerchantId, int? merchantId, string? merchantType, string? countryIso3, bool? status)
         {
             var response = new ApiResponse<IEnumerable<MerchantDto>>();
 
@@ -125,7 +125,7 @@ namespace Fincompare.Application.Services
                     merchants = merchants.Where(mp => mp.Country3Iso == countryIso3);
                 if (status.HasValue)
                     merchants = merchants.Where(mp => mp.Status == status.Value);
-                if (status.HasValue)
+                if (!string.IsNullOrEmpty(merchantType))
                     merchants = merchants.Where(mp => mp.MerchantType == merchantType);
 
                 var merchantsResponse = _mapper.Map<IEnumerable<MerchantDto>>(merchants);
